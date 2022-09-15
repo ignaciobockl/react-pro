b9bb240c928b6b6c78996f7bc4b29c89d61f74b4
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-09-15 17:50:01 -0300

    asignar componentes a otro componente - implementacion de interface

--- src/02-component-patterns/interfaces/index.ts
+++ src/02-component-patterns/interfaces/index.ts
@@ -6,13 +6,20 @@ interface Product {
     img?: string;
 };
 
+export interface ProductCardProps {
+    children?: ReactElement | ReactElement[]; // tambien puede ser () => JSX.Element
+    product: Product;
+};
+
+export interface ProductCardHOCProps {
+    ({ children, product }: ProductCardProps): JSX.Element,
+    Buttons: () => JSX.Element,
+    Image: ({ img }: { img?: string }) => JSX.Element,
+    Title: ({ title }: { title?: string }) => JSX.Element,
+}
+
 export interface ProductContextProps {
     counter: number;
     increaseBy: (value: number) => void;
     product: Product;
 }
-
-export interface ProductCardProps {
-    children?: ReactElement | ReactElement[]; // tambien puede ser () => JSX.Element
-    product: Product;
-};
